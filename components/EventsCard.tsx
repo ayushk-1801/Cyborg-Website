5716b684c05bd071f81d8a8c569af411ef8cf464
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-12-24 21:25:13 +0530

    fixed event card

--- components/EventsCard.tsx
+++ components/EventsCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Event() {
   return (
-    <div className="w-full text-white">
+    <div className="w-full text-white transition-all hover:scale-105">
       <Image
         src="/home.svg"
         alt="Home"
@@ -14,11 +14,11 @@ export default function Event() {
         Event Name
       </div>
 
-      <div className="grid grid-cols-3 rounded-xl overflow-hidden">
-        <div className="bg-lavender-900 text-center font-bold text-6xl rounded-l-xl">
+      <div className="grid grid-cols-3 rounded-bl-xl rounded-br-xl overflow-hidden">
+        <div className="bg-lavender-900 text-center font-bold text-6xl h-full flex items-center justify-center">
           26
         </div>
-        <div className="col-span-2 text-center font-semibold text-4xl bg-lavender-700 rounded-r-xl">
+        <div className="col-span-2 text-center font-semibold text-4xl bg-lavender-700 p-2">
           January <br />
           2024
         </div>
